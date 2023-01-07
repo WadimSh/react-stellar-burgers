2858689d5445512b67c0a66533ccefe59fc6141f
WadimSh <wadim11@yandex.ru>
2023-01-07 21:41:21 +0300

    fix: изменен компонент burger-ingredients-card

--- src/components/burger-ingredients-card/burger-ingredients-card.tsx
+++ src/components/burger-ingredients-card/burger-ingredients-card.tsx
@@ -1,16 +1,19 @@
-import { useMemo } from "react";
+import { useMemo, FC } from "react";
 import { useSelector } from "react-redux";
 import { useDrag } from "react-dnd";
 import { Link, useLocation, } from "react-router-dom";
-import PropTypes from 'prop-types';
 
+import { TIngredient } from '../../types/types';
 import {Counter, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
-import { messagePropTypes } from '../../utils/messagePropTypes';
 import style from './burger-ingredients-card.module.css';
 
-function BurgerIngredientsCard({ element }) {
+interface IIngredientsItems {
+  element: TIngredient;
+}
+
+const BurgerIngredientsCard: FC<IIngredientsItems> = ({ element }) => {
   const location = useLocation();
-  const { bun, ingredients } = useSelector((store) => store.currentBurger);
+  const { bun, ingredients } = useSelector((store: any) => store.currentBurger);
  
   const [{ opacity }, dragRef] = useDrag({
     type: "ingredient",
@@ -50,8 +53,4 @@ function BurgerIngredientsCard({ element }) {
   )
 }
 
-BurgerIngredientsCard.propTypes = {
-  element: PropTypes.objectOf(messagePropTypes).isRequired
-}
-                      
 export default BurgerIngredientsCard;
