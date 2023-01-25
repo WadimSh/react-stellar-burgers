b7ab055d06b846f39e03b5b500e1f4c673a49283
WadimSh <wadim11@yandex.ru>
2023-01-25 13:23:43 +0300

    fix

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,16 +1,10 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
-import { getUser } from '../../services/actions/auth-actions';
-import { useAppSelector, useAppDispatch } from "../../hooks/hooks";
+import { useAppSelector } from "../../hooks/hooks";
 
 const ProtectedRoute: FC<RouteProps> = ({ children, ...rest }) => {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-      dispatch(getUser())
-  }, []);
-
   const { isAuth } = useAppSelector((store) => store.auth);
-  console.log(isAuth)
+  
   return (
     <Route
       {...rest}
