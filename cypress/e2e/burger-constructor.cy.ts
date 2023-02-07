2dac6e574ece71cb376b7d097739165b7e9db4c8
WadimSh <wadim11@yandex.ru>
2023-02-07 09:37:17 +0300

    fix

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -4,12 +4,12 @@ describe('service is available', function() {
     cy.intercept("POST", "api/orders", { fixture: "orders.json" });
     cy.viewport(1450, 1100)
     cy.visit('http://localhost:3000');
+    cy.wait(1000);
   })
 
   
   it('should be available on localhost:3000', function() {
     cy.get('[data-cy="item-ingredient"]').contains('Краторная булка N-200i').click();
-    cy.wait(1000);
     cy.get('[data-cy="modal"]').contains("Детали ингредиента");
     cy.wait(2000);
     cy.get('[data-cy="modal-close"]').click();
@@ -34,11 +34,11 @@ describe('service is available', function() {
     cy.wait(1000);
     cy.contains('button', 'Войти').click();
     cy.location('pathname', { timeout: 1000 }).should('eq', '/');
-    
-    cy.get("button").contains("Оформить заказ").click();
     cy.wait(2000);
+
+    cy.get("button").contains("Оформить заказ").click();
     cy.get('[data-cy="modal-close"]').click();
-    cy.wait(1000);
+    cy.wait(2000);
   })
   
   
